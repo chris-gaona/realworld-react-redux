3fd843b20392b01c54b30b339fce565e8e77c619
Chris Gaona <thebeloved88@sbcglobal.net>
2017-10-29 11:50:48 -0700

    Wires up the login form

--- src/agent.js
+++ src/agent.js
@@ -9,12 +9,19 @@ const responseBody = res => res.body;
 
 const requests = {
     get: url =>
-        superagent.get(`${API_ROOT}${url}`).then(responseBody)
+        superagent.get(`${API_ROOT}${url}`).then(responseBody),
+    post: (url, body) =>
+        superagent.post(`${API_ROOT}${url}`, body).then(responseBody)
 };
 
 const Articles = {
     all: page =>
         requests.get(`/articles?limit=10`)
+};
+
+const Auth = {
+    login: (email, password) =>
+        requests.post('/users/login', {user: {email, password}})
 };
 
 export default {
