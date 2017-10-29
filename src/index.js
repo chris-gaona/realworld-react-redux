20d3d1e8a7a4bef56d8942d2846870630f96829b
Chris Gaona <thebeloved88@sbcglobal.net>
2017-10-28 18:02:31 -0700

    Creates a basic react-redux project

--- src/index.js
+++ src/index.js
@@ -15,13 +15,37 @@ const reducer = function (state = defaultState, action) {
 const store = createStore(reducer);
 
 class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    componentWillMount() {
+        store.subscribe(() => this.setState(store.getState()));
+    }
+
+    render() {
+        const onClick = () => store.dispatch({type: 'TOGGLE'});
+
+        return (
+            <div>
+                <h1>To-dos</h1>
+                <div>
+                    Learn Redux&nbsp;
+                    <input
+                        type="checkbox"
+                        checked={!!this.state.checked}
+                        onClick={onClick}
+                        />
+                </div>
+                {
+                    this.state.checked ? (<h2>Done!</h2>) : null
+                }
+            </div>
+        );
+    }
 }
 
 ReactDOM.render((
-  <App />
+    <App />
 ), document.getElementById('root'));
