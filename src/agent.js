0bfdac2ac6ce8c145fcf1a2fbab5788549ea103c
Chris Gaona <thebeloved88@sbcglobal.net>
2017-10-31 19:37:55 -0700

    Adding delete functionality for articles

--- src/agent.js
+++ src/agent.js
@@ -18,14 +18,20 @@ const requests = {
     get: url =>
         superagent.get(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
     post: (url, body) =>
-        superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
+        superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody),
+    del: url =>
+        superagent.del(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+    put: (url, body) =>
+        superagent.put(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
 };
 
 const Articles = {
     all: page =>
         requests.get(`/articles?limit=10`),
     get: slug =>
-        requests.get(`/articles/${slug}`)
+        requests.get(`/articles/${slug}`),
+    del: slug =>
+        requests.del(`/articles/${slug}`)
 };
 
 const Comments = {
