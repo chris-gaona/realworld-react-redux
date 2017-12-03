d0d7fbd3b5099972e12f9d82a2d0fdf9bbbd50c9
Chris Gaona <thebeloved88@sbcglobal.net>
2017-12-03 10:21:01 -0800

    Adds pagination to articles lists

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,32 +1,38 @@
+import ArticlePreview from './ArticlePreview';
+import ListPagination from './ListPagination';
 import React from 'react';
-import ArticlePreview from "./ArticlePreview";
 
 const ArticleList = props => {
-    if (!props.articles) {
-        return (
-            <div className="article-preview">Loading...</div>
-        );
-    }
-
-    if (props.articles.length === 0) {
-        return (
-            <div className="article-preview">
-                No articles are here...yet.
-            </div>
-        );
-    }
-
+  if (!props.articles) {
     return (
-        <div>
-            {
-                props.articles.map(article => {
-                    return (
-                        <ArticlePreview article={article} key={article.slug}/>
-                    );
-                })
-            }
-        </div>
-    )
+      <div className="article-preview">Loading...</div>
+    );
+  }
+
+  if (props.articles.length === 0) {
+    return (
+      <div className="article-preview">
+        No articles are here... yet.
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {
+        props.articles.map(article => {
+          return (
+            <ArticlePreview article={article} />
+          );
+        })
+      }
+
+      <ListPagination
+        articlesCount={props.articlesCount}
+        currentPage={props.currentPage}
+        onSetPage={props.onSetPage} />
+    </div>
+  );
 };
 
 export default ArticleList;
